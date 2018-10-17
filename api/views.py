ba35e07a0442091d1253bc70a746fe829db23334
robin-zhu <1365076679@qq.com>
2018-10-17 23:15:40 +0800

    init
    初始化

---
+++ api/views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets
+from .models import Student
+from .serializers import StudentSerializers
+
+# Create your views here.
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    # 指定结果并设置排序
+    queryset = Student.objects.all().order_by('-pk')
+    # 指定序列化的类
+    serializer_class = StudentSerializers
